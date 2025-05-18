5af1f575be551a91559c7d1efd0550507f4a0c4b
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2025-05-18 16:36:23 +0500

    chat history

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -1,11 +1,18 @@
 import chainlit as cl
 from rich import print
 from my_secrets import Secrets
-from agents import Agent, OpenAIChatCompletionsModel, AsyncOpenAI, set_tracing_disabled, Runner
+from agents import (
+    Agent,
+    OpenAIChatCompletionsModel,
+    AsyncOpenAI,
+    set_tracing_disabled,
+    Runner,
+)
+from typing import cast
 
 
-@cl.on_message
-async def main(msg: cl.Message):
+@cl.on_chat_start
+async def start():
     secrets = Secrets()
     external_client = AsyncOpenAI(
         base_url=secrets.gemini_api_url,
@@ -13,21 +20,42 @@ async def main(msg: cl.Message):
     )
     set_tracing_disabled(True)
     agent = Agent(
-        name = "Assistant",
+        name="Assistant",
         instructions="Answer the question as best as you can.",
-        model = OpenAIChatCompletionsModel(
-            model=secrets.gemini_api_model,
-            openai_client=external_client
-        )
-          
+        model=OpenAIChatCompletionsModel(
+            model=secrets.gemini_api_model, openai_client=external_client
+        ),
     )
-    result = Runner.run_sync(
-        starting_agent=agent,
-        input=msg.content
+
+    cl.user_session.set("agent", agent)
+
+    cl.user_session.set("chat_history", [])
+
+    await cl.Message(
+        content="Gemini API Chatbot",
+    ).send()
+
+
+@cl.on_message
+async def main(msg: cl.Message):
+
+    agent = cast(Agent, cl.user_session.get("agent"))
+
+    chat_history: list = cl.user_session.get("chat_history") or []
+
+    chat_history.append(
+        {
+            "role": "user",
+            "content": msg.content,
+        }
     )
+
+    result = Runner.run_sync(starting_agent=agent, input=chat_history)
     
-    message = cl.Message(
-        content=result.final_output
-    )
+    cl.user_session.set("chat_history",result.to_input_list())
     
+    print(chat_history)
+
+    message = cl.Message(content=result.final_output)
+
     await message.send()
